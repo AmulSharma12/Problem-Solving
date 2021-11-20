2522085f3888ebab10594cea91c24d3f9fb4638e
Amul Op <amulsharmarr67@gmail.com>
2021-11-20 16:05:28 +0530

    InsertionSort

--- 15-Sorting/InsertionSort.java
+++ 15-Sorting/InsertionSort.java
@@ -1,18 +1,50 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+
+class Insertion
+{
+	public  Insertion(int[] arr)
+	{
+		this.insertion(arr);
+	}
+
+
+	private static void insertion(int[] nums)
+	{
+		int lastIndex = nums.length-2;
+		for(int i = 0; i<lastIndex; i++)
+		{
+			int nextIndex = i+1;
+			for(int currIndex = nextIndex; currIndex>0; currIndex--)
+			{
+				int prev_Index = currIndex-1;
+				if(nums[currIndex]< nums[prev_Index])
+					swap(nums,currIndex,prev_Index);
+				else
+					break;
+			}
+		}
+	}
+
+	//swap function
+
+	private static void swap(int[] arr, int first, int last)
+	{
+		int temp = arr[first];
+		arr[first] = arr[last];
+		arr[last] = temp;
+	}
+}
+
+
 public class InsertionSort
 {
 	public static void main(String[] args)
 	{
 		int[] arr = {3,3,6,78,9};
-		insertion();
+		Insertion insSort = new Insertion(arr);
+		// insertion(arr);
 		System.out.println(Arrays.toString(arr));
-	}
-
-
-	public static void insertion(int[] nums)
-	{
-		
 	}
 }
